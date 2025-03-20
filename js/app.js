0a1f82c838ab18ec93942c6551dc66d18af0974e
MojtabaNabid <mojtaba.nabid@gmail.com>
2025-03-20 15:31:54 +0100

    search by Name is done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("hi");
+const searchByName = document.querySelector(".searchByName");
+
+const searchByNameHandler = (event) => {
+  console.log(event.target.value);
+  const cart = document.getElementsByClassName("cart");
+  console.log(cart);
+  for (let product of cart) {
+    if(event.target.value === "") {
+        product.classList.remove("hidden")
+    }else {
+        if (!product.getElementsByClassName('productName')[0].innerText.includes(event.target.value)) {
+            product.classList.add("hidden")
+        } 
+    }
+  }
+};
+searchByName.addEventListener("input", searchByNameHandler);
