e1968ec190e3af1834ead7d86c856b25e1dcd6b5
MojtabaNabid <mojtaba.nabid@gmail.com>
2025-03-21 18:45:22 +0100

    searchByNameHandler improved

--- js/app.js
+++ js/app.js
@@ -3,44 +3,37 @@ const all = document.querySelector(".searchAll");
 const tech = document.querySelector(".searchTech");
 const clothing = document.querySelector(".searchClothing");
 const health = document.querySelector(".searchHealth");
-const priceButton = document.querySelector(".priceButton")
-const range = document.querySelector(".priceRange")
-const minValue = document.getElementById("minValue")
-const maxValue = document.getElementById("maxValue")
-const currentValue = document.getElementById("currentValue")
-const carts = document.getElementsByClassName("cart")
+const priceButton = document.querySelector(".priceButton");
+const range = document.querySelector(".priceRange");
+const minValue = document.getElementById("minValue");
+const maxValue = document.getElementById("maxValue");
+const currentValue = document.getElementById("currentValue");
+const carts = document.getElementsByClassName("cart");
 
-
-let prices = []
+let prices = [];
 for (product of carts) {
-    prices.push(parseInt(product.querySelector(".price").innerText.split(" ")[1]))
+  prices.push(
+    parseInt(product.querySelector(".price").innerText.split(" ")[1])
+  );
 }
-const maxPrice = Math.max(...prices)
+const maxPrice = Math.max(...prices);
 // console.log("The maximum price in this page: ",maxPrice);
-range.setAttribute("max", maxPrice)
+range.setAttribute("max", maxPrice);
 minValue.innerText = 0;
 maxValue.innerText = maxPrice + " $";
 // console.log(prices.sort((a,b) => b-a));
 
 const searchByNameHandler = (event) => {
-  //   console.log(event.target.value);
-  const value = event.target.value.toLowerCase();
-  console.log(value);
-  //   console.log(cart);
+  const searchValue = event.target.value.toLowerCase().trim();
   for (let product of carts) {
-    if (event.target.value === "") {
-      product.classList.remove("hidden");
+    productName = product.children[1].innerText.toLowerCase();
+    if(productName.includes(searchValue)) {
+        product.style.display = "block"
     } else {
-      if (
-        !product
-          .getElementsByClassName("productName")[0]
-          .innerText.toLowerCase()
-          .includes(value)
-      ) {
-        product.classList.add("hidden");
-      }
+        product.style.display = "none"
     }
-  }
+  };
+  
 };
 
 const searchAllHandler = (event) => {
@@ -84,42 +77,47 @@ const searchHealthHandler = (event) => {
 };
 
 const priceSearchHandler = (event) => {
-    const searchInput = document.querySelector(".searchByPrice")
-    
-    for (let product of carts) {
-            if (searchInput.value === "" || parseInt(searchInput.value) === "NaN") {
-                product.classList.remove("hidden")
-            } else {
-                let price = parseInt(product.querySelector(".price").innerText.split(" ")[1])
-                // console.log(price);
-                if (price === +searchInput.value) {
-                    product.classList.remove("hidden")
-                } else {
-                    product.classList.add("hidden")
-                }
-            }
-        }
-    
-    // prices.push(product.innerText.split(" "))
-    // prices = prices.flat().filter((item) => item !== "$");
-    // console.log(searchInput.value);
-}
+  const searchInput = document.querySelector(".searchByPrice");
+
+  for (let product of carts) {
+    if (searchInput.value === "" || parseInt(searchInput.value) === "NaN") {
+      product.classList.remove("hidden");
+    } else {
+      let price = parseInt(
+        product.querySelector(".price").innerText.split(" ")[1]
+      );
+      // console.log(price);
+      if (price === +searchInput.value) {
+        product.classList.remove("hidden");
+      } else {
+        product.classList.add("hidden");
+      }
+    }
+  }
+
+  // prices.push(product.innerText.split(" "))
+  // prices = prices.flat().filter((item) => item !== "$");
+  // console.log(searchInput.value);
+};
 
 const rangeHandler = (event) => {
-    currentValue.innerText = event.target.value  + " $"
-    // console.log("range: ",event.target.value);
-    for (let product of carts) {
-        // console.log(product.querySelector(".price").innerText.split(" ")[1]);
-        if (+product.querySelector(".price").innerText.split(" ")[1] <= +event.target.value){
-            product.classList.remove("hidden")
-    } else product.classList.add("hidden")
-}
-}
+  currentValue.innerText = event.target.value + " $";
+  // console.log("range: ",event.target.value);
+  for (let product of carts) {
+    // console.log(product.querySelector(".price").innerText.split(" ")[1]);
+    if (
+      +product.querySelector(".price").innerText.split(" ")[1] <=
+      +event.target.value
+    ) {
+      product.classList.remove("hidden");
+    } else product.classList.add("hidden");
+  }
+};
 
 searchByName.addEventListener("input", searchByNameHandler);
 all.addEventListener("click", searchAllHandler);
 tech.addEventListener("click", searchTechHandler);
 clothing.addEventListener("click", searchClothingHandler);
 health.addEventListener("click", searchHealthHandler);
-priceButton.addEventListener("click", priceSearchHandler)
-range.addEventListener("input", rangeHandler)
+priceButton.addEventListener("click", priceSearchHandler);
+range.addEventListener("input", rangeHandler);
